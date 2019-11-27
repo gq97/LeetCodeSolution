642a4779a1a7981476962f3c16fa9cc8819c4730
gq97 <18270847603@163.com>
2019-11-27 15:43:54 +0800

    Add solution for LeetCode230

---
+++ src/LeetCode230_KthSmallestElementInABST.java
@@ -0,0 +1,27 @@
+/*
+ * Definition for a binary tree node.
+ */
+class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+}
+
+
+public class LeetCode230_KthSmallestElementInABST {
+    // A better idea is to use inorder travel
+
+    public int countNode(TreeNode root) {
+        return root == null ? 0 : 1 + countNode(root.left) + countNode(root.right);
+    }
+
+    public int kthSmallest(TreeNode root, int k) {
+        if(root == null) return 0;
+        int leftCount = countNode(root.left);
+        if(leftCount + 1 > k) return kthSmallest(root.left, k);
+        else if(leftCount + 1 == k) {
+            return root.val;
+        }
+        return kthSmallest(root.right, k - leftCount - 1);
+    }
+}
